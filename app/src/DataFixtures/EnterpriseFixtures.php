06f3cf076891870918aea505c98f5f5f071d6376
Gregory SIMON <simon.24260@gmail.com>
2023-06-27 14:51:14 +0200

    Fixtures user enterprise etu et offre bon pour la demo

--- app/src/DataFixtures/EnterpriseFixtures.php
+++ app/src/DataFixtures/EnterpriseFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Enterprise;
+use App\Entity\Offers;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -16,7 +17,7 @@ class EnterpriseFixtures extends Fixture implements DependentFixtureInterface {
             ->setEmail("enterprise1@mail.com")
             ->setName("Thalès")
             ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. ")
-            ->setUser($this->getReference(UserFixtures::USER1))
+            ->setUser($this->getReference(UserFixtures::USER_ENTERPRISE1))
             ->setCity("Bordeaux")
             ->setDepartment("33000")
             ->setImageName("test")
@@ -29,7 +30,7 @@ class EnterpriseFixtures extends Fixture implements DependentFixtureInterface {
     {
         return [
             StatusFixtures::class,
-            UserFixtures::class
+            UserFixtures::class,
         ];
     }
 }
--- app/src/DataFixtures/UserFixtures.php
+++ app/src/DataFixtures/UserFixtures.php
@@ -8,7 +8,10 @@ use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture {
-    public const USER1 = 'user1';
+
+    public const USER_ADMIN = 'admin';
+    public const USER_ENTERPRISE1 = 'user_enterprise1';
+    public const USER_STUDENT1 = 'user_student1';
     private UserPasswordHasherInterface $hasher;
 
     public function __construct(UserPasswordHasherInterface  $passwordHasher)
@@ -18,13 +21,34 @@ class UserFixtures extends Fixture {
 
     public function load(ObjectManager $manager)
     {
-        $user = (new User())
+        $userAdm = (new User())
+            ->setEmail("admin@mail.com")
+            ->setIsVerified(true)
+            ->setImageFilename("test")
+            ->setRoles(["ROLE_ADMIN"]);
+        $userAdm->setPassword($this->hasher->hashPassword($userAdm, 'admin'));
+        $this->addReference(self::USER_ADMIN, $userAdm);
+        $this->setReference(self::USER_ADMIN, $userAdm);
+        $manager->getRepository(User::class)->save($userAdm, true);
+
+        $userEnterprise = (new User())
             ->setEmail("enterprise1@mail.com")
             ->setIsVerified(true)
             ->setImageFilename("test")
             ->setRoles(["ROLE_ENTERPRISE"]);
-        $user->setPassword($this->hasher->hashPassword($user, 'user1'));
-        $this->addReference(self::USER1, $user);
-        $manager->getRepository(User::class)->save($user, true);
+        $userEnterprise->setPassword($this->hasher->hashPassword($userEnterprise, 'user1'));
+        $this->addReference(self::USER_ENTERPRISE1, $userEnterprise);
+        $this->setReference(self::USER_ENTERPRISE1, $userEnterprise);
+        $manager->getRepository(User::class)->save($userEnterprise, true);
+
+        $userEtu = (new User())
+            ->setEmail("etu1@mail.com")
+            ->setIsVerified(true)
+            ->setImageFilename("test")
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu->setPassword($this->hasher->hashPassword($userEtu, 'etu1'));
+        $this->addReference(self::USER_STUDENT1, $userEtu);
+        $this->setReference(self::USER_STUDENT1, $userEtu);
+        $manager->getRepository(User::class)->save($userEtu, true);
     }
 }
