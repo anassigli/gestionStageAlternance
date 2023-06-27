9f37241d6a95715f684f20a991f8bcf16fa283da
Gregory SIMON <simon.24260@gmail.com>
2023-06-27 15:16:56 +0200

    Jeux de données pret pour demain

---
+++ app/src/DataFixtures/StudentsFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Student;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class StudentsFixtures extends Fixture implements DependentFixtureInterface
+{
+    public const STUDENT1 = 'student1';
+    public function load(ObjectManager $manager)
+    {
+        $student = (new Student())
+            ->setEmail("etu1@mail.com")
+            ->setUser($this->getReference(UserFixtures::USER_STUDENT1))
+            ->setFirstname("Toto")
+            ->setLastname("Titi");
+        $this->addReference(self::STUDENT1, $student);
+        $this->setReference(self::STUDENT1, $student);
+        $manager->getRepository(Student::class)->save($student, true);
+    }
+
+    public function getDependencies()
+    {
+        return [
+            UserFixtures::class
+        ];
+    }
+}
