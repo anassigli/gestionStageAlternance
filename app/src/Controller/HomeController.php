b9596513284ce39f882e45bb153e634945ac9d4b
trainteau <tom.rainteau@etu.u-bordeaux.fr>
2023-06-28 11:40:04 +0200

    changement des boutons sur carte en fonction de l'utilisateur

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Offers;
+use App\Entity\User;
 use App\Repository\OffersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,7 +14,17 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(OffersRepository $offersRepository): Response
     {
+        /** @var User $current_user */
+        $current_user = $this->getUser();
+        $current_user_id = $current_user->getId();
+        $current_user_role = $current_user->getRoles();
+        $candidacies = $current_user->getStudent()->getCandidacies();
         $offers = $offersRepository->findAll();
-        return $this->render('home/index.html.twig',['offers'=>$offers]);
+        return $this->render('home/index.html.twig',[
+            'offers'=>$offers,
+            'current_user_role'=>$current_user_role,
+            'candidacies'=>$candidacies,
+            'current_user_id'=>$current_user_id
+        ]);
     }
 }
