86b92b7648642e4e6988b3733f5b3bf725d904a4
anass igli <aigli@u-bordeaux.fr>
2023-06-29 08:57:40 +0200

    working on charts

--- app/src/Controller/Admin/AdminController.php
+++ app/src/Controller/Admin/AdminController.php
@@ -6,6 +6,7 @@ use App\Entity\Student;
 use App\Entity\Tags;
 use App\Entity\Candidacy;
 use App\Entity\User;
+use App\Repository\EnterpriseRepository;
 use App\Repository\StudentRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
@@ -14,23 +15,50 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class AdminController extends AbstractDashboardController
 {
     private StudentRepository $studentRepository;
+    private ChartBuilderInterface $chartBuilder;
+    private EnterpriseRepository $entrepriseRepository;
 
-    public function __construct(StudentRepository $studentRepository)
+    public function __construct(StudentRepository $studentRepository ,EnterpriseRepository $entrepriseRepository, ChartBuilderInterface $chartBuilder)
     {
         $this->studentRepository = $studentRepository;
+        $this->chartBuilder = $chartBuilder;
+        $this->entrepriseRepository = $entrepriseRepository;
     }
 
     #[IsGranted('ROLE_ADMIN')]
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-
+        $er = $this->entrepriseRepository->findTodaysNewCompanies();
         $sr = $this->studentRepository->findAll();
-        return $this->render('admin/index.html.twig' , ['students'=>$sr]);
+
+        $chart2 = $this->chartBuilder->createChart(Chart::TYPE_PIE);
+        $chart2->setData([
+            'labels' => ['Nouveaux étudiants',"Nouvelles entreprises"],
+            'datasets' => [
+                [
+                    'labels' => ['Nouveaux Candidats ' , 'Nouvelles Entreprises'],
+                    'backgroundColor' => ['rgb(255, 99, 132, .4)','rgb(120, 99, 132, .4)'],
+                    'borderColor' => 'rgb(255, 99, 132)',
+                    'data' => [2, 7],
+                    'tension' => 0.4,
+                ],
+            ],
+        ]);
+
+        $chart2->setOptions([
+            'maintainAspectRatio' => false,
+        ]);
+
+        return $this->render('admin/index.html.twig' , ['sr'=>$sr,'chart2'=>$chart2]);
+
+
     }
 
     public function configureDashboard(): Dashboard
@@ -47,5 +75,13 @@ class AdminController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Gestion Des Candidatures', 'fas fa-list', Candidacy::class);
         yield MenuItem::linkToCrud('Gestion Des Utilisateurs', 'fas fa-list', User::class);
         yield MenuItem::linkToCrud('Gestion Des Tags', 'fas fa-list', Tags::class);
+    }
+
+
+
+    public function configureAssets(): \EasyCorp\Bundle\EasyAdminBundle\Config\Assets
+    {
+        return parent::configureAssets()
+            ->addWebpackEncoreEntry('app');
     }
 }
