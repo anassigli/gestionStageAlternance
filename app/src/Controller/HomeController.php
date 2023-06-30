84e2936b529af12b1b1ef7dcecd196bdcdce0300
trainteau <tom.rainteau@etu.u-bordeaux.fr>
2023-06-30 11:51:17 +0200

    recherche par tags

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use App\Entity\Offers;
 use App\Entity\User;
 use App\Form\EnterpriseFormType;
 use App\Form\StudentFormType;
+use App\Repository\CategoryRepository;
 use App\Repository\EnterpriseRepository;
 use App\Repository\OffersRepository;
 use App\Repository\StudentRepository;
@@ -21,21 +22,24 @@ class HomeController extends AbstractController
     private EnterpriseRepository $enterpriseRepository;
     private StudentRepository $studentRepository;
     private OffersRepository $offersRepository;
+    private CategoryRepository $categoryRepository;
 
     public function __construct(EnterpriseRepository $enterpriseRepository,
                                 StudentRepository    $studentRepository,
-                                OffersRepository     $offersRepository)
+                                OffersRepository     $offersRepository,
+                                CategoryRepository   $categoryRepository)
     {
         $this->enterpriseRepository = $enterpriseRepository;
         $this->studentRepository = $studentRepository;
         $this->offersRepository = $offersRepository;
+        $this->categoryRepository = $categoryRepository;
     }
 
     #[Route('/', name: 'app_home')]
     public function index(Session $session, Request $request): Response
     {
         $offers = $this->offersRepository->findAll();
-
+        $categories = $this->categoryRepository->findAll();
         if ($this->getUser()) {
             /** @var User $current_user */
             $current_user = $this->getUser();
@@ -57,6 +61,7 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'offers' => $offers,
             'candidacies' => $candidacies ?? null,
+            'categories' => $categories,
         ]);
     }
 
@@ -74,7 +79,7 @@ class HomeController extends AbstractController
         $formStudent = $this->createForm(StudentFormType::class, $student);
 
         if (isset($enterprise)) {
-            $offers = $offersRepository->findBy(["enterprise" => $enterprise]);
+            $offers = $this->offersRepository->findBy(["enterprise" => $enterprise]);
             $nbTotalCandidacies = 0;
             foreach ($offers as $offer){
                 $nbTotalCandidacies += $offer->getCandidacies()->count();
