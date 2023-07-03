fa7836b0a038963fb90917a6628b013bde3415ee
trainteau <tom.rainteau@etu.u-bordeaux.fr>
2023-07-03 14:51:33 +0200

    ajout d'étudiantet d'entreprise

--- app/src/DataFixtures/OffersFixtures.php
+++ app/src/DataFixtures/OffersFixtures.php
@@ -11,64 +11,147 @@ class OffersFixtures extends Fixture implements DependentFixtureInterface
 {
     public const OFFER_ENTERPRISE1_1 = 'offer_enterprise1_1';
     public const OFFER_ENTERPRISE1_2 = 'offer_enterprise1_2';
-    public const OFFER_ENTERPRISE1_3 = 'offer_enterprise1_3';
-    public const OFFER_ENTERPRISE1_4 = 'offer_enterprise1_4';
-
     public const OFFER_ENTERPRISE2_1 = 'offer_enterprise2_1';
     public const OFFER_ENTERPRISE2_2 = 'offer_enterprise2_2';
+    public const OFFER_ENTERPRISE3_1 = 'offer_enterprise3_1';
+    public const OFFER_ENTERPRISE3_2 = 'offer_enterprise3_2';
+    public const OFFER_ENTERPRISE4_1 = 'offer_enterprise4_1';
+    public const OFFER_ENTERPRISE4_2 = 'offer_enterprise4_2';
+    public const OFFER_ENTERPRISE5_1 = 'offer_enterprise5_1';
+    public const OFFER_ENTERPRISE5_2 = 'offer_enterprise5_2';
 
     public function load(ObjectManager $manager)
     {
-        for($i = 1; $i < 5; $i++) {
-            $offer = (new Offers())
-                ->setName("Offre d'alternance de développeur FullStack " . $i)
-                ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
-                ->setDepartment("33000")
-                ->setCity("Bordeaux")
-                ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE1))
-                ->setStatus($this->getReference(StatusFixtures::VERIFIED));
 
-            switch ($i){
-                case 2:
-                    $this->addReference(self::OFFER_ENTERPRISE1_2, $offer);
-                    $this->setReference(self::OFFER_ENTERPRISE1_2, $offer);
-                    break;
-                case 3:
-                    $this->addReference(self::OFFER_ENTERPRISE1_3, $offer);
-                    $this->setReference(self::OFFER_ENTERPRISE1_3, $offer);
-                    break;
-                case 4:
-                    $this->addReference(self::OFFER_ENTERPRISE1_4, $offer);
-                    $this->setReference(self::OFFER_ENTERPRISE1_4, $offer);
-                    break;
-                default:
-                    $this->addReference(self::OFFER_ENTERPRISE1_1, $offer);
-                    $this->setReference(self::OFFER_ENTERPRISE1_1, $offer);
-                    break;
-            }
-            $manager->getRepository(Offers::class)->save($offer, true);
-        }
-        for($i = 1; $i < 3; $i++) {
-            $offer = (new Offers())
-                ->setName("Offre d'alternance de développeur FullStack " . $i)
-                ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
-                ->setDepartment("33160")
-                ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE2))
-                ->setCity("Cestas")
-                ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+        $offer1_1 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " . "jbedzbvzej efzu fhzb")
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33000")
+            ->setCity("Bordeaux")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE1))
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
 
-            switch ($i){
-                case 2:
-                    $this->addReference(self::OFFER_ENTERPRISE2_2, $offer);
-                    $this->setReference(self::OFFER_ENTERPRISE2_2, $offer);
-                    break;
-                default:
-                    $this->addReference(self::OFFER_ENTERPRISE2_1, $offer);
-                    $this->setReference(self::OFFER_ENTERPRISE2_1, $offer);
-                    break;
-            }
-            $manager->getRepository(Offers::class)->save($offer, true);
-        }
+        $offer1_2 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " )
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33160")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE1))
+            ->setCity("Cestas")
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+
+
+        $offer2_1 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " . "jbedzbvzej efzu fhzb")
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33000")
+            ->setCity("Bordeaux")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE2))
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+
+
+        $offer2_2 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " )
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33160")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE2))
+            ->setCity("Cestas")
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+
+
+        $offer3_1 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " . "jbedzbvzej efzu fhzb")
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33000")
+            ->setCity("Bordeaux")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE3))
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+
+
+        $offer3_2 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " )
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33160")
+            ->setCity("Cestas")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE3))
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+
+
+        $offer4_1 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " . "jbedzbvzej efzu fhzb")
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33000")
+            ->setCity("Bordeaux")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE4))
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+
+
+        $offer4_2 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " )
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33160")
+            ->setCity("Cestas")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE4))
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+
+
+        $offer5_1 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " . "jbedzbvzej efzu fhzb")
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33000")
+            ->setCity("Bordeaux")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE5))
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+
+
+        $offer5_2 = (new Offers())
+            ->setName("Offre d'alternance de développeur FullStack " )
+            ->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae enim quam. Nulla maximus nisi felis, eget ornare massa accumsan tempor. Vivamus tempus venenatis turpis, vitae porttitor justo. Maecenas interdum gravida scelerisque. Nunc egestas purus quis consectetur pellentesque. Sed ut efficitur neque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi non blandit diam. Vivamus sit amet neque eu ex pharetra imperdiet. Vestibulum sagittis pellentesque erat sit amet aliquam. In sit amet odio felis. Vestibulum tincidunt massa lorem, et luctus orci rhoncus eu. Quisque in elit tincidunt, tincidunt mauris ac, mattis arcu. Nullam faucibus sem vel orci congue imperdiet. Mauris cursus tempus aliquam. ")
+            ->setDepartment("33160")
+            ->setCity("Cestas")
+            ->setEnterprise($this->getReference(EnterpriseFixtures::ENTERPRISE5))
+            ->setStatus($this->getReference(StatusFixtures::VERIFIED));
+
+
+        $this->addReference(self::OFFER_ENTERPRISE1_1, $offer1_1);
+        $this->setReference(self::OFFER_ENTERPRISE1_1, $offer1_1);
+
+        $this->addReference(self::OFFER_ENTERPRISE1_2, $offer1_2);
+        $this->setReference(self::OFFER_ENTERPRISE1_2, $offer1_2);
+
+        $this->addReference(self::OFFER_ENTERPRISE2_1, $offer2_1);
+        $this->setReference(self::OFFER_ENTERPRISE2_1, $offer2_1);
+
+        $this->addReference(self::OFFER_ENTERPRISE2_2, $offer2_2);
+        $this->setReference(self::OFFER_ENTERPRISE2_2, $offer2_2);
+
+        $this->addReference(self::OFFER_ENTERPRISE3_1, $offer3_1);
+        $this->setReference(self::OFFER_ENTERPRISE3_1, $offer3_1);
+
+        $this->addReference(self::OFFER_ENTERPRISE3_2, $offer3_2);
+        $this->setReference(self::OFFER_ENTERPRISE3_2, $offer3_2);
+
+        $this->addReference(self::OFFER_ENTERPRISE4_1, $offer4_1);
+        $this->setReference(self::OFFER_ENTERPRISE4_1, $offer4_1);
+
+        $this->addReference(self::OFFER_ENTERPRISE4_2, $offer4_2);
+        $this->setReference(self::OFFER_ENTERPRISE4_2, $offer4_2);
+
+        $this->addReference(self::OFFER_ENTERPRISE5_1, $offer5_1);
+        $this->setReference(self::OFFER_ENTERPRISE5_1, $offer5_1);
+
+        $this->addReference(self::OFFER_ENTERPRISE5_2, $offer5_2);
+        $this->setReference(self::OFFER_ENTERPRISE5_2, $offer5_2);
+
+        $manager->getRepository(Offers::class)->save($offer2_2, true);
+        $manager->getRepository(Offers::class)->save($offer5_2, true);
+        $manager->getRepository(Offers::class)->save($offer4_1, true);
+        $manager->getRepository(Offers::class)->save($offer1_1, true);
+        $manager->getRepository(Offers::class)->save($offer5_1, true);
+        $manager->getRepository(Offers::class)->save($offer1_2, true);
+        $manager->getRepository(Offers::class)->save($offer3_2, true);
+        $manager->getRepository(Offers::class)->save($offer4_2, true);
+        $manager->getRepository(Offers::class)->save($offer3_1, true);
+        $manager->getRepository(Offers::class)->save($offer2_1, true);
     }
 
     public function getDependencies(): array
--- app/src/DataFixtures/UserFixtures.php
+++ app/src/DataFixtures/UserFixtures.php
@@ -18,6 +18,21 @@ class UserFixtures extends Fixture {
     public const USER_STUDENT1 = 'user_student1';
     public const USER_STUDENT2 = 'user_student2';
     public const USER_STUDENT3 = 'user_student3';
+    public const USER_STUDENT4 = 'user_student4';
+    public const USER_STUDENT5 = 'user_student5';
+    public const USER_STUDENT6 = 'user_student6';
+    public const USER_STUDENT7 = 'user_student7';
+    public const USER_STUDENT8 = 'user_student8';
+    public const USER_STUDENT9 = 'user_student9';
+    public const USER_STUDENT10 = 'user_student10';
+    public const USER_STUDENT11 = 'user_student11';
+    public const USER_STUDENT12 = 'user_student12';
+    public const USER_STUDENT13 = 'user_student13';
+    public const USER_STUDENT14 = 'user_student14';
+    public const USER_STUDENT15 = 'user_student15';
+    public const USER_STUDENT16 = 'user_student16';
+
+
     private UserPasswordHasherInterface $hasher;
 
     public function __construct(UserPasswordHasherInterface  $passwordHasher)
@@ -72,14 +87,14 @@ class UserFixtures extends Fixture {
         $this->setReference(self::USER_ENTERPRISE4, $userEnterprise4);
         $manager->getRepository(User::class)->save($userEnterprise4, true);
 
-        $userEnterprise4 = (new User())
-            ->setEmail("enterprise4@mail.com")
+        $userEnterprise5 = (new User())
+            ->setEmail("enterprise5@mail.com")
             ->setIsVerified(true)
             ->setRoles(["ROLE_ENTERPRISE"]);
-        $userEnterprise2->setPassword($this->hasher->hashPassword($userEnterprise2, 'enterprise4'));
-        $this->addReference(self::USER_ENTERPRISE4, $userEnterprise4);
-        $this->setReference(self::USER_ENTERPRISE4, $userEnterprise4);
-        $manager->getRepository(User::class)->save($userEnterprise4, true);
+        $userEnterprise5->setPassword($this->hasher->hashPassword($userEnterprise5, 'enterprise5'));
+        $this->addReference(self::USER_ENTERPRISE5, $userEnterprise5);
+        $this->setReference(self::USER_ENTERPRISE5, $userEnterprise5);
+        $manager->getRepository(User::class)->save($userEnterprise5, true);
 
         $userEtu1 = (new User())
             ->setEmail("etu1@mail.com")
@@ -107,5 +122,131 @@ class UserFixtures extends Fixture {
         $this->addReference(self::USER_STUDENT3, $userEtu3);
         $this->setReference(self::USER_STUDENT3, $userEtu3);
         $manager->getRepository(User::class)->save($userEtu3, true);
+
+        $userEtu4 = (new User())
+            ->setEmail("etu4@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu4->setPassword($this->hasher->hashPassword($userEtu4, 'etu4'));
+        $this->addReference(self::USER_STUDENT4, $userEtu4);
+        $this->setReference(self::USER_STUDENT4, $userEtu4);
+        $manager->getRepository(User::class)->save($userEtu4, true);
+
+        $userEtu5 = (new User())
+            ->setEmail("etu5@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu5->setPassword($this->hasher->hashPassword($userEtu5, 'etu5'));
+        $this->addReference(self::USER_STUDENT5, $userEtu5);
+        $this->setReference(self::USER_STUDENT5, $userEtu5);
+        $manager->getRepository(User::class)->save($userEtu5, true);
+
+        $userEtu6 = (new User())
+            ->setEmail("etu6@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu6->setPassword($this->hasher->hashPassword($userEtu6, 'etu6'));
+        $this->addReference(self::USER_STUDENT3, $userEtu6);
+        $this->setReference(self::USER_STUDENT3, $userEtu6);
+        $manager->getRepository(User::class)->save($userEtu6, true);
+
+        $userEtu7 = (new User())
+            ->setEmail("etu7@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu7->setPassword($this->hasher->hashPassword($userEtu7, 'etu7'));
+        $this->addReference(self::USER_STUDENT7, $userEtu7);
+        $this->setReference(self::USER_STUDENT7, $userEtu7);
+        $manager->getRepository(User::class)->save($userEtu7, true);
+
+        $userEtu8 = (new User())
+            ->setEmail("etu8@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu8->setPassword($this->hasher->hashPassword($userEtu8, 'etu8'));
+        $this->addReference(self::USER_STUDENT8, $userEtu8);
+        $this->setReference(self::USER_STUDENT8, $userEtu8);
+        $manager->getRepository(User::class)->save($userEtu8, true);
+
+        $userEtu9 = (new User())
+            ->setEmail("etu9@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu9->setPassword($this->hasher->hashPassword($userEtu9, 'etu9'));
+        $this->addReference(self::USER_STUDENT9, $userEtu9);
+        $this->setReference(self::USER_STUDENT9, $userEtu9);
+        $manager->getRepository(User::class)->save($userEtu9, true);
+
+        $userEtu10 = (new User())
+            ->setEmail("etu10@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu10->setPassword($this->hasher->hashPassword($userEtu10, 'etu10'));
+        $this->addReference(self::USER_STUDENT10, $userEtu10);
+        $this->setReference(self::USER_STUDENT10, $userEtu10);
+        $manager->getRepository(User::class)->save($userEtu10, true);
+
+        $userEtu11 = (new User())
+            ->setEmail("etu11@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu11->setPassword($this->hasher->hashPassword($userEtu11, 'etu11'));
+        $this->addReference(self::USER_STUDENT11, $userEtu11);
+        $this->setReference(self::USER_STUDENT11, $userEtu11);
+        $manager->getRepository(User::class)->save($userEtu11, true);
+
+        $userEtu12 = (new User())
+            ->setEmail("etu12@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu12->setPassword($this->hasher->hashPassword($userEtu12, 'etu12'));
+        $this->addReference(self::USER_STUDENT12, $userEtu12);
+        $this->setReference(self::USER_STUDENT12, $userEtu12);
+        $manager->getRepository(User::class)->save($userEtu12, true);
+
+        $userEtu12 = (new User())
+            ->setEmail("etu12@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu12->setPassword($this->hasher->hashPassword($userEtu12, 'etu12'));
+        $this->addReference(self::USER_STUDENT12, $userEtu12);
+        $this->setReference(self::USER_STUDENT12, $userEtu12);
+        $manager->getRepository(User::class)->save($userEtu12, true);
+
+        $userEtu13 = (new User())
+            ->setEmail("etu13@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu13->setPassword($this->hasher->hashPassword($userEtu13, 'etu13'));
+        $this->addReference(self::USER_STUDENT13, $userEtu13);
+        $this->setReference(self::USER_STUDENT13, $userEtu13);
+        $manager->getRepository(User::class)->save($userEtu13, true);
+
+        $userEtu14 = (new User())
+            ->setEmail("etu13@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu14->setPassword($this->hasher->hashPassword($userEtu14, 'etu14'));
+        $this->addReference(self::USER_STUDENT14, $userEtu14);
+        $this->setReference(self::USER_STUDENT14, $userEtu14);
+        $manager->getRepository(User::class)->save($userEtu14, true);
+
+        $userEtu15 = (new User())
+            ->setEmail("etu15@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu15->setPassword($this->hasher->hashPassword($userEtu15, 'etu15'));
+        $this->addReference(self::USER_STUDENT3, $userEtu15);
+        $this->setReference(self::USER_STUDENT3, $userEtu15);
+        $manager->getRepository(User::class)->save($userEtu15, true);
+
+        $userEtu16 = (new User())
+            ->setEmail("etu16@mail.com")
+            ->setIsVerified(true)
+            ->setRoles(["ROLE_STUDENT"]);
+        $userEtu16->setPassword($this->hasher->hashPassword($userEtu16, 'etu16'));
+        $this->addReference(self::USER_STUDENT16, $userEtu16);
+        $this->setReference(self::USER_STUDENT16, $userEtu16);
+        $manager->getRepository(User::class)->save($userEtu16, true);
     }
 }
