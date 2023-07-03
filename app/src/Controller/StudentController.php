ac306cfaeb48846968dff06ecb8f30b48c539601
anass igli <aigli@u-bordeaux.fr>
2023-07-03 17:39:14 +0200

    rebase

--- app/src/Controller/StudentController.php
+++ app/src/Controller/StudentController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Student;
 use App\Form\StudentFormType;
+use App\Repository\CandidacyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,12 +13,15 @@ class StudentController extends AbstractController
 {
     #[Route('/student/{id}/show', name: 'app_student_show_profile')]
     public function showProfile(
-        Student $student
+        Student             $student,
+        CandidacyRepository $candidacyRepository
     ): Response
     {
+        $candidacy = $candidacyRepository->findOneBy(["student"=> $student]);
         $formStudent = $this->createForm(StudentFormType::class, $student);
         return $this->render('student/show.html.twig', [
             'form' => $formStudent,
+            'candidacy' => $candidacy
         ]);
     }
 }
